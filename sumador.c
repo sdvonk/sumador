41e0a31ec2bc467a9fe16438a443b0885162e52f
sdvonk <balersoro@gmail.com>
2026-04-22 09:01:32 -0300

    segundo commit

--- sumador.c
+++ sumador.c
@@ -3,15 +3,18 @@
 #define s scanf
 int main(void)
 {
-    int num1, num2, suma;
+    int num1, num2, suma, res;
 
+    //holaa//
     p("ingrese un valor: ");
     s("%d", &num1);
     p("ingrese otro valor: ");
     s("%d", &num2);
 
     suma = num1 + num2;
+    p("sum) %d + %d = %d    ", num1, num2, suma);
 
-    p("%d + %d = %d", num1, num2, suma);
+    res = num1 - num2;
+    p("resta) %d - %d = %d", num1, num2, res);
     return 0;
 }
